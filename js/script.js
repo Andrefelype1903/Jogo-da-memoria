9bed1dd8cbce49541599a8cb2425af5dc84520c0
andrefelype <andrefilype21122014@gmail.com>
2023-07-13 21:22:39 -0300

    jogo da memoria

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+
+const form = document.querySelector('.login-form');
+
+input.addEventListener('input', ({target}) =>  {
+    if(target.value.length >= 3) {
+        button.removeAttribute('disabled');
+        return;
+    } 
+
+    button.setAttribute('disabled', '');
+})
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = 'pages/game.html'
+    
+})
+
